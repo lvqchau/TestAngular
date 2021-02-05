f73383918ef6256372626516b8dbae36dfad029e
Quỳnh Châu <lvqchau@apcs.vn>
2021-02-05 21:35:48 +0700

    angular

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { SignupParams, SignupResult } from './../../core/model/user.model';
 
 @Component({
   selector: 'app-signup',
@@ -9,17 +11,23 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit {
   @ViewChild('signupForm') signupForm!: NgForm
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   handleSignup() {
     // Làm sao lấy đc value của form, sử dụng ViewChild
-
     if(this.signupForm.invalid) return
 
-    alert(JSON.stringify(this.signupForm.value))
+    this.authService.signup(this.signupForm.value).subscribe({
+      next: (result) => {
+        console.log(result)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
   }
 
 }
