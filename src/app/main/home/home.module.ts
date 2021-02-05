7bbee24637a3fa669aa75b0ee9034757a7eabd54
Quỳnh Châu <lvqchau@apcs.vn>
2021-02-05 20:25:30 +0700

    angular

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -6,12 +6,16 @@ import { HomeComponent } from './home.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [HomeComponent, CarouselComponent, MovieListComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatGridListModule,
+    MatCardModule
   ]
 })
 export class HomeModule { }
