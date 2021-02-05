9d2f2b8b714f175912634e2fbf585de5f8d73ca5
Quỳnh Châu <lvqchau@apcs.vn>
2021-02-05 21:03:59 +0700

    angular

--- src/app/main/movie/movie.component.ts
+++ src/app/main/movie/movie.component.ts
@@ -1,16 +1,29 @@
+import { MovieDetail } from './../../core/model/movie.model';
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../core/services/movie.service'
 import { MovieDetail } from '../../core/model/movie.model'
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  movieDetail: MovieDetail | null = null
 
-  constructor(private movieService: MovieService) { }
+  constructor(
+    private movieService: MovieService,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    //B1: Lấy maPhim từ url
+    this.activatedRoute.params.subscribe({
+      next: (params) => {
+        console.log(params)
+      }
+    })
+    //B2: Gọi api
   }
 
 }
