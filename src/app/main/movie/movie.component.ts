df0eeb8bb77f6a7ecb3d680252f001efe6932804
Quỳnh Châu <lvqchau@apcs.vn>
2021-02-05 21:16:35 +0700

    angular

--- src/app/main/movie/movie.component.ts
+++ src/app/main/movie/movie.component.ts
@@ -1,6 +1,7 @@
+import { ShowTimes } from './../../core/model/movie.model';
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../core/services/movie.service'
-import { MovieDetail } from '../../core/model/movie.model'
+import { Movie, MovieDetail } from '../../core/model/movie.model'
 import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-movie',
@@ -8,7 +9,8 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
-  movieDetail: MovieDetail | null = null
+  movieDetail: Movie | null = null
+  showTimes: ShowTimes[] = []
 
   constructor(
     private movieService: MovieService,
@@ -19,10 +21,13 @@ export class MovieComponent implements OnInit {
     //B1: Lấy maPhim từ url
     this.activatedRoute.params.subscribe({
       next: (params) => {
-        //B2: Gọi api
+        //B2: Dùng maPhim gọi api lấy thông tin phim
         this.movieService.getMovieDetail(params.movieId).subscribe({
           next: (result) => {
-            console.log(result)
+            const { lichChieu, ...detail } = result
+            this.movieDetail = detail
+            this.showTimes = lichChieu
+            console.log(this.movieDetail)
           },
           error: (error) => {
             console.log(error)
